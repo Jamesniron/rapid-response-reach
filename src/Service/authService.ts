522e246d909370554ca9b341df294f3b8e72368a
Nithushan UCA <nithshan@unicornconnectedapps.com>
2025-08-07 22:04:41 +0530

    fixed base url bug

--- src/Service/authService.ts
+++ src/Service/authService.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5046/api';
+ const API_BASE_URL = 'https://localhost:7032/api';
 
 export const loginUser = async (email: string, password: string) => {
-  const response = await axios.post(`${API_BASE_URL}/User/login?email=${email}&password=${password}`, {
+  const response = await axios.post(`${API_BASE_URL}/User/login`, {
+    email,
+    password
   });
 
   return response.data;
